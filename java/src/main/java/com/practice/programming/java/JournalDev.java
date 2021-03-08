ea4a0c16a5d307ab6826b1894b8ddb9ab678e60d
vyom <vyom.reloaded@gmail.com>
2021-03-07 19:13:27 -0500

    prime check

--- java/src/main/java/com/practice/programming/java/JournalDev.java
+++ java/src/main/java/com/practice/programming/java/JournalDev.java
@@ -18,6 +18,13 @@ public class JournalDev {
         checkVowels("random string");
         checkVowels("tmz");
         printLineBreak();
+        // prime check
+        checkPrime(12343);
+        checkPrime(15);
+        checkPrime(19);
+        checkPrime(41);
+        checkPrime(7865429);
+        printLineBreak();
     }
 
     private static void printLineBreak() {
@@ -58,5 +65,36 @@ public class JournalDev {
     public static void checkVowels(String input) {
         boolean containsVowels = input.toLowerCase().matches(".+[aeiou].+");
         System.out.printf("Does input string : '%s' has any vowel - %b %n", input, containsVowels);
+    }
+
+    /**
+     * prime check. TODO: can this be optimized?
+     */
+    public static void checkPrime(long input) {
+        boolean isPrime = true;
+        if (input <= 1) {
+            System.out.printf("%d is not prime %n", input);
+            return;
+        } else if (input <= 3) {
+            System.out.printf("%d is prime %n", input);
+            return;
+        } else if (input % 2 == 0 || input % 3 == 0) {
+            System.out.printf("%d is not prime %n", input);
+            return;
+        } else {
+            for (int i = 5; i * i <= input; i = i + 6) {
+                // after 5. validate with +2 to check against 7
+                // +6 will give 11, +2 check with 13
+                // +6 will give 17, +2 check with 19
+                // +6 will give 23
+                if (input % i == 0 || input % (i + 2) == 0)
+                    isPrime = false;
+            }
+        }
+        if (isPrime)
+            System.out.printf("%d is prime %n", input);
+        else
+            System.out.printf("%d is not prime %n", input); 
+
     }
 }
