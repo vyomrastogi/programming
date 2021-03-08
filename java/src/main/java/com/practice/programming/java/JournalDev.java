7e567fec83bf6579df6c6b86efe30064ec84ffa3
vyom <vyom.reloaded@gmail.com>
2021-03-07 19:54:15 -0500

    fibonacci using stream

--- java/src/main/java/com/practice/programming/java/JournalDev.java
+++ java/src/main/java/com/practice/programming/java/JournalDev.java
@@ -1,6 +1,10 @@
 package com.practice.programming.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JournalDev {
 
@@ -24,6 +28,14 @@ public class JournalDev {
         checkPrime(19);
         checkPrime(41);
         checkPrime(7865429);
+        printLineBreak();
+        // fibonacci
+        printFibonacci(10);
+        printFibonacci(20);
+        printLineBreak();
+        // fibonacci using stream
+        printFibonacciUsingStream(13);
+        printFibonacciUsingStream(25);
         printLineBreak();
     }
 
@@ -94,7 +106,42 @@ public class JournalDev {
         if (isPrime)
             System.out.printf("%d is prime %n", input);
         else
-            System.out.printf("%d is not prime %n", input); 
+            System.out.printf("%d is not prime %n", input);
 
+    }
+
+    /**
+     * Fibonacci
+     */
+    private static void printFibonacci(int limit) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        List<Integer> fibonacciList = new ArrayList<>();
+        for (int i = 0; i <= limit; i++) {
+            fibonacciList.add(a);
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        System.out.printf("Fibonacci : %s %n",
+                fibonacciList.stream().map(String::valueOf).collect(Collectors.joining(",")));
+    }
+
+    /**
+     * Fibonacci using stream
+     */
+    private static void printFibonacciUsingStream(int limit) {
+        System.out.printf("Fibonacci: %s %n", fibonacciString(limit));
+    }
+
+    private static String fibonacciString(int limit) {
+        return Stream
+                .iterate(new long[] { 0, 1 },
+                        previousPair -> new long[] { previousPair[1], previousPair[0] + previousPair[1] })
+                .limit(limit)//limit iterations
+                .map(pair -> pair[0]) //get 0th element from pair
+                .map(String::valueOf) // conver to string
+                .collect(Collectors.joining(",")); // comma separated 
     }
 }
